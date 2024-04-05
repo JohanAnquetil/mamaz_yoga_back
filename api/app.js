04aebab2ac8cd1a81c59fca1f5c78da64e650e79
Johan <anquetil.johan@laposte.net>
2024-04-05 11:56:42 +0200

    datas

--- api/app.js
+++ api/app.js
@@ -4,10 +4,16 @@ const { success } = require('./helper.js');
 const { sequelize, db } = require('./config/sequelize.js');
 const Post = require('./src/models/posts.js');
 const PostMeta = require('./src/models/posts_meta.js');
+const User = require('./src/models/users.js');
+const Member = require('./src/models/members.js');
 
 // Appel des routes
 require('./routes/get_published_posts')(app);
 require('./routes/get_published_posts_by_pk')(app);
+require('./routes/get_users')(app);
+require('./routes/get_members')(app);
+require('./routes/get_member_by_mail.js')(app);
+require('./routes/get_authentification.js')(app);
 
 // Démarrage du serveur Express
 const port = process.env.PORT || 3000;
