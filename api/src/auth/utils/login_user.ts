572e8e02e5fe01a8b83e7f4f38a56e82fdee7703
Johan Anquetil <114992907+JohanAnquetil@users.noreply.github.com>
2025-11-21 15:00:58 +0100

    async pb bug

--- api/src/auth/utils/login_user.ts
+++ api/src/auth/utils/login_user.ts
@@ -2,8 +2,8 @@ import { HttpException } from "@nestjs/common";
 import { verifyPassword } from "./hash_verify_password";
 
 // Returns if the user entered his password comparing with a hash
-function loginUser(password: string, storedHash: string): boolean {
-  const isValid = verifyPassword(password, storedHash);
+async function loginUser(password: string, storedHash: string): Promise<boolean> {
+  const isValid = await verifyPassword(password, storedHash);
   if (!isValid) {
     throw new HttpException(`Utilisateur ou mot de passe inconnu`, 401);
   }
